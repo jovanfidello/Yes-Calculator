118ce8d1bade75e271a55e65e2f4e71a95faac2f
jovanfidello <96285805+jovanfidello@users.noreply.github.com>
2022-12-06 01:36:08 +0700

    Delete betacalcu directory

--- betacalcu/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-for (int ikd = 1; ikd<=base;ikd++) {
-                                        //     scanf(" %c", storage[ikd]);
-                                        //     for (int jkd=1; jkd<=base; jkd++) {
-                                        //        char storageangka[] = myAtoi(storage[jkd+1]);
-                                        //     }
-                                            
-                                        // }
